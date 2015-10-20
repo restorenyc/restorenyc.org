f1b3561b98ca2617c11d00cce9d62915f7c0f230
John Kim <john.kimz@gmail.com>
2015-10-20 15:31:14 -0400

    added banner lead

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,26 @@
 var Utils = (function() {
+	$(document).ready(function() {
+		Utils.init();
+	});
+
 	return {
 		$html: $('html'),
 		$body: $('body'),
 		$main: $('#main'),
-		$nav: $('#site-nav')
+		$nav: $('#site-nav'),
+		init: function() {
+			Utils.removeBgVideo();
+		},
+		isMobile: function() {
+			var ua = navigator.userAgent,
+				pattern = /Android|iPhone|iPad|PlayBook|Mobile|IEMobile/i;
+			return pattern.test(ua);
+		},
+		removeBgVideo: function() {
+			if(Utils.isMobile()) {
+				$('.bg-video').remove();
+			}
+		}
 	};
 })(jQuery);
 (function($) {
